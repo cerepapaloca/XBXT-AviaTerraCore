c15a9f2737873aa530d4c1c493cb3c9f36ac46f1
Ceres <cagutierrezpayares@gmail.com>
2024-10-08 07:10:28 -0500

    Comienzo

---
+++ Avia Terra Core/src/main/java/net/atcore/avia/AviaTerraCore.java
@@ -0,0 +1,17 @@
+package net.atcore.avia;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AviaTerraCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
